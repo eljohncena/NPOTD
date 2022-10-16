dcb5ee13fc064cecf5eba8cc189c086c39abd15a
John Chavez <70674723+eljohncena@users.noreply.github.com>
2022-10-15 21:50:12 -0400

    Add files via upload

---
+++ NPOTD/NPOTD/NPOTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  NPOTDApp.swift
+//  NPOTD
+//
+//  Created by John Chavez on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct NPOTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

1b717f2b28f483043758e5dc71ab01e67571963c
John Chavez <70674723+eljohncena@users.noreply.github.com>
2022-10-15 21:48:40 -0400

    Add files via upload
    App is working and calls nasa api to display image using demo key. Need to refactor code.

---
+++ NPOTD/NPOTD/PhotoInfo.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoInfo.swift
+//  NPOTD
+//
+//  Created by John Chavez on 10/15/22.
+//
+
+import Foundation
+
+struct PhotoInfo: Codable {
+    var title: String
+    var description: String
+    var url: URL
+    var copyright: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description = "explanation"
+        case url
+        case copyright
+    }
+}
